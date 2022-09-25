07a8034a57ec11f321a4acb5969c04bf7f9bae61
igrmm <igrmmdev@gmail.com>
2022-09-25 19:57:43 -0400

    add BoundingBoxComponent

---
+++ core/src/com/igrmm/igt/components/BoundingBoxComponent.java
@@ -0,0 +1,8 @@
+package com.igrmm.igt.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Rectangle;
+
+public class BoundingBoxComponent implements Component {
+	public final Rectangle bBox = new Rectangle();
+}
