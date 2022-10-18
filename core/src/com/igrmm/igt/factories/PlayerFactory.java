6643d82069ef8d21f98f3f1dbde1ed13a9dd5e4f
igrmm <igrmmdev@gmail.com>
2022-10-17 21:39:10 -0400

    add AnimationComponent to player

--- core/src/com/igrmm/igt/factories/PlayerFactory.java
+++ core/src/com/igrmm/igt/factories/PlayerFactory.java
@@ -2,20 +2,30 @@ package com.igrmm.igt.factories;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.igrmm.igt.AsepriteAnimation;
+import com.igrmm.igt.Assets;
+import com.igrmm.igt.components.AnimationComponent;
 import com.igrmm.igt.components.BoundingBoxComponent;
 import com.igrmm.igt.components.MovementComponent;
 
 public class PlayerFactory {
-	public static Entity createPlayer(Engine engine) {
+	public static Entity createPlayer(Engine engine, Assets assets) {
 		Entity playerEntity = engine.createEntity();
 		engine.addEntity(playerEntity);
 
+		//Components dependencies
+		AsepriteAnimation asepriteAnimation = assets.getAsepriteAnimation("player");
+
 		//Create components
 		playerEntity.add(new BoundingBoxComponent());
-		MovementComponent playerMovementC = new MovementComponent();
-		playerEntity.add(playerMovementC);
+		playerEntity.add(new MovementComponent());
+		playerEntity.add(new AnimationComponent(asepriteAnimation));
 
 		//Tweak numbers
+		MovementComponent playerMovementC = playerEntity.getComponent(MovementComponent.class);
+		AnimationComponent playerAnimationC = playerEntity.getComponent(AnimationComponent.class);
+
+		playerAnimationC.currentAnimation = "idle_right";
 		playerMovementC.maxSpeed = 240f;
 		playerMovementC.acceleration = 1080f;
 		playerMovementC.friction = 1080f;
