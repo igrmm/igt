bc3a70f6b6f30a2ad78b8f9bd8a3a4292623c1db
igrmm <igrmmdev@gmail.com>
2022-10-24 21:07:22 -0400

    add loaded field to save class

--- core/src/com/igrmm/igt/Save.java
+++ core/src/com/igrmm/igt/Save.java
@@ -5,7 +5,17 @@ import com.igrmm.igt.components.StatisticsComponent;
 import com.igrmm.igt.components.MapComponent;
 
 public class Save {
+	private transient boolean loaded = false;
+
 	public final StatisticsComponent statisticsComponent = new StatisticsComponent();
 	public final SpawnPointComponent spawnPointComponent = new SpawnPointComponent();
 	public final MapComponent mapComponent = new MapComponent();
+
+	public boolean isLoaded() {
+		return this.loaded;
+	}
+
+	public void setLoaded(boolean loaded) {
+		this.loaded = loaded;
+	}
 }
