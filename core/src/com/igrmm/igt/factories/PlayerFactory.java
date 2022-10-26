0244f7a84a0e4071117ce77bf0ddf94c272f32c8
igor.mamedes <igrmmdev@gmail.com>
2022-10-26 18:05:00 -0400

    make player spawn at saved spawn point

--- core/src/com/igrmm/igt/factories/PlayerFactory.java
+++ core/src/com/igrmm/igt/factories/PlayerFactory.java
@@ -2,12 +2,18 @@ package com.igrmm.igt.factories;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.math.Rectangle;
 import com.igrmm.igt.AsepriteAnimation;
 import com.igrmm.igt.Assets;
 import com.igrmm.igt.Save;
 import com.igrmm.igt.components.AnimationComponent;
+import com.igrmm.igt.components.SpawnPointComponent;
 import com.igrmm.igt.components.boundingboxes.BoundingBoxComponent;
 import com.igrmm.igt.components.MovementComponent;
+import com.igrmm.igt.components.boundingboxes.SpawnPointBoundingBoxComponent;
+
+import java.util.Objects;
 
 public class PlayerFactory {
 	public static Entity createPlayer(Engine engine, Assets assets) {
@@ -37,6 +43,22 @@ public class PlayerFactory {
 		playerMovC.maxSpeed = 240f;
 		playerMovC.acceleration = 1080f;
 		playerMovC.friction = 1080f;
+
+		//make player spawn at saved spawn point
+		SpawnPointComponent playerSpawnPointC = playerEntity.getComponent(SpawnPointComponent.class);
+		BoundingBoxComponent playerBboxC = playerEntity.getComponent(BoundingBoxComponent.class);
+		Rectangle playerBbox = playerBboxC.bbox;
+		for (Entity spawnPointE : engine.getEntitiesFor(Family.one(SpawnPointBoundingBoxComponent.class).get())) {
+			SpawnPointComponent spawnPointC = spawnPointE.getComponent(SpawnPointComponent.class);
+			if (Objects.equals(playerSpawnPointC.name, spawnPointC.name)) {
+				SpawnPointBoundingBoxComponent spawnPointBboxC
+						= spawnPointE.getComponent(SpawnPointBoundingBoxComponent.class);
+				Rectangle spawnPointBbox = spawnPointBboxC.bbox;
+
+				playerBbox.x = spawnPointBbox.x;
+				playerBbox.y = spawnPointBbox.y;
+			}
+		}
 
 		return playerEntity;
 	}
