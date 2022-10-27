db072a2cf23c4bf06f0a136bc06a470ff8f9f8b9
igrmm <igrmmdev@gmail.com>
2022-10-26 23:19:35 -0400

    make TimeTrackingSystem use component mapper

--- core/src/com/igrmm/igt/systems/TimeTrackingSystem.java
+++ core/src/com/igrmm/igt/systems/TimeTrackingSystem.java
@@ -1,5 +1,6 @@
 package com.igrmm.igt.systems;
 
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.igrmm.igt.components.StatisticsComponent;
@@ -8,7 +9,8 @@ public class TimeTrackingSystem extends EntitySystem {
 	private final StatisticsComponent playerStatisticsC;
 
 	public TimeTrackingSystem(Entity playerE) {
-		playerStatisticsC = playerE.getComponent(StatisticsComponent.class);
+		ComponentMapper<StatisticsComponent> statisticsM = ComponentMapper.getFor(StatisticsComponent.class);
+		playerStatisticsC = statisticsM.get(playerE);
 	}
 
 	@Override
