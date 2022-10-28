b3bb8f8d1aae13e86524a9e21f2b2ca561089178
igor.mamedes <igrmmdev@gmail.com>
2022-10-28 18:07:00 -0400

    remove BlockBoundingBoxComponent

--- core/src/com/igrmm/igt/components/boundingboxes/BlockBoundingBoxComponent.java
+++
@@ -1,7 +0,0 @@
-package com.igrmm.igt.components.boundingboxes;
-
-public class BlockBoundingBoxComponent extends BoundingBoxComponent {
-	public BlockBoundingBoxComponent(float x, float y, float width, float height) {
-		super(x, y, width, height);
-	}
-}
