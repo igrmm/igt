a429267b8aade27bbcd5cb4e80ee0eff7f2ad51c
igrmm <igrmmdev@gmail.com>
2022-10-27 22:29:04 -0400

    add SpawnPointEntityFactory class

---
+++ core/src/com/igrmm/igt/factories/SpawnPointEntityFactory.java
@@ -0,0 +1,34 @@
+package com.igrmm.igt.factories;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.utils.XmlReader;
+import com.igrmm.igt.components.EntityTypeComponent;
+import com.igrmm.igt.components.SpawnPointComponent;
+
+import java.util.Objects;
+
+public class SpawnPointEntityFactory implements EntityFactory {
+	@Override
+	public Entity createEntity(MapObject mapObject) {
+		Entity entity = EntityFactory.super.createEntity(mapObject);
+		entity.add(new SpawnPointETComponent());
+		return entity;
+	}
+
+	@Override
+	public Component createComponent(String propertyType, XmlReader.Element componentPropertiesXml) {
+		if ("spawnPointComponent".equals(propertyType)) {
+			SpawnPointComponent spawnPointC = new SpawnPointComponent();
+			XmlReader.Element spawnPointNameXml = componentPropertiesXml.getChild(0);
+			if (Objects.equals(spawnPointNameXml.getAttribute("name"), "name"))
+				spawnPointC.name = spawnPointNameXml.getAttribute("value", "start");
+			return spawnPointC;
+		}
+		throw new Error("Component not found = " + propertyType);
+	}
+
+	public static class SpawnPointETComponent extends EntityTypeComponent {
+	}
+}
