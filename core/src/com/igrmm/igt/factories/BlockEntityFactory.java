58e81afb674950087f0779d2bcfdf172f177b1f7
igor.mamedes <igrmmdev@gmail.com>
2022-10-28 18:02:00 -0400

    remove not needed override

--- core/src/com/igrmm/igt/factories/BlockEntityFactory.java
+++ core/src/com/igrmm/igt/factories/BlockEntityFactory.java
@@ -1,9 +1,7 @@
 package com.igrmm.igt.factories;
 
-import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.utils.XmlReader;
 import com.igrmm.igt.components.EntityTypeComponent;
 
 public class BlockEntityFactory implements EntityFactory {
@@ -12,11 +10,6 @@ public class BlockEntityFactory implements EntityFactory {
 		Entity entity = EntityFactory.super.createEntity(mapObject);
 		entity.add(new BlockETComponent());
 		return entity;
-	}
-
-	@Override
-	public Component createComponent(String propertyType, XmlReader.Element componentPropertiesXml) {
-		throw new Error("Component not found = " + propertyType);
 	}
 
 	public static class BlockETComponent extends EntityTypeComponent {
