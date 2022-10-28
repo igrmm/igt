f38da9b56595fd5e71e8217bbcd3d59926f07078
igor.mamedes <igrmmdev@gmail.com>
2022-10-28 18:04:00 -0400

    split TiledMapEntityFactory into separate factories; implements EntityFac.

--- core/src/com/igrmm/igt/factories/TiledMapEntityFactory.java
+++ core/src/com/igrmm/igt/factories/TiledMapEntityFactory.java
@@ -1,70 +1,21 @@
 package com.igrmm.igt.factories;
 
-import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.utils.XmlReader;
-import com.igrmm.igt.components.*;
-import com.igrmm.igt.components.boundingboxes.*;
 
-import java.util.Iterator;
-import java.util.Objects;
-
-public class TiledMapEntityFactory {
-	public static Entity createEntity(MapObject mapObject) {
-		Entity entity = new Entity();
-		MapProperties properties = mapObject.getProperties();
-
-		//get bounding box component
-		BoundingBoxComponent bboxC = getBboxC(
-				properties.get("entityType", String.class),
-				properties.get("x", Float.class),
-				properties.get("y", Float.class),
-				properties.get("width", Float.class),
-				properties.get("height", Float.class)
-		);
-		entity.add(bboxC);
-
-		//get other components
-		XmlReader xmlReader = new XmlReader();
-		Iterator<String> iterator = properties.getKeys();
-		while (iterator.hasNext()) {
-			String key = iterator.next();
-			Object value = properties.get(key);
-			if (Objects.equals(key, "componentXml")) {
-				XmlReader.Element componentXml = xmlReader.parse(value.toString());
-				Component component = getComponent(componentXml);
-				entity.add(component);
-			}
-		}
-
-		return entity;
-	}
-
-	private static BoundingBoxComponent getBboxC(String entityType, Float x, Float y, Float width, Float height) {
+public class TiledMapEntityFactory implements EntityFactory {
+	@Override
+	public Entity createEntity(MapObject mapObject) {
+		String entityType = mapObject.getProperties().get("entitytype", String.class);
 		switch (entityType) {
 			case "block":
-				return new BlockBoundingBoxComponent(x, y, width, height);
+				BlockEntityFactory blockEntityFactory = new BlockEntityFactory();
+				return blockEntityFactory.createEntity(mapObject);
 			case "spawn-point":
-				return new SpawnPointBoundingBoxComponent(x, y, width, height);
+				SpawnPointEntityFactory spawnPointEntityFactory = new SpawnPointEntityFactory();
+				return spawnPointEntityFactory.createEntity(mapObject);
 			default:
-				return new BoundingBoxComponent(x, y, width, height);
-		}
-	}
-
-	private static Component getComponent(XmlReader.Element componentXml) {
-		XmlReader.Element properties = componentXml.getChildByName("properties");
-		String propertyType = componentXml.get("propertytype");
-		switch (propertyType) {
-			case "spawnPointComponent":
-				SpawnPointComponent spawnPointC = new SpawnPointComponent();
-				XmlReader.Element spawnPointNameXml = properties.getChild(0);
-				if (Objects.equals(spawnPointNameXml.getAttribute("name"), "name"))
-					spawnPointC.name = spawnPointNameXml.getAttribute("value", "start");
-				return spawnPointC;
-			default:
-				throw new Error("Component not found");
+				throw new RuntimeException("Entity not implemented: " + entityType);
 		}
 	}
 }
