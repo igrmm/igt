a825fba0346aaa2260a44ec13483c4131322297b
igrmm <igrmmdev@gmail.com>
2022-11-08 22:08:32 -0400

    remove old comment

--- core/src/com/igrmm/igt/components/BoundingBoxComponent.java
+++ core/src/com/igrmm/igt/components/BoundingBoxComponent.java
@@ -3,10 +3,6 @@ package com.igrmm.igt.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Rectangle;
 
-/**
- * Generic BoundingBox component containing a rectangle. Unique bounding boxes can be created by extending this class,
- * making it easy to identify entities.
- */
 public class BoundingBoxComponent implements Component {
 	public final Rectangle bbox;
 
